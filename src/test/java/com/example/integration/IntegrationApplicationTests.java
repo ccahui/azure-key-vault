01c1529c581a593341c62037696a61da984fcd4e
ccahui <kris_ti4n@hotmail.com>
2024-11-28 22:53:23 -0500

    Configuration test local

--- src/test/java/com/example/integration/IntegrationApplicationTests.java
+++ src/test/java/com/example/integration/IntegrationApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.integration;
 
+import com.example.integration.config.ValuesProperties;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
-@SpringBootTest
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest(properties = "spring.config.name=application-test")
 class IntegrationApplicationTests {
 
+	@MockBean
+	private ValuesProperties keyVaultService;
 	@Test
 	void contextLoads() {
+		when(keyVaultService.getLocalValue()).thenReturn("mocked-secret");
 	}
 
 }
