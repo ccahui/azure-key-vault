d10e776ec03424c3fdc9f5b9baee2987dc12cd40
ccahui <kris_ti4n@hotmail.com>
2024-12-04 23:05:58 -0500

    Update Test

--- src/test/java/com/example/integration/IntegrationApplicationTests.java
+++ src/test/java/com/example/integration/IntegrationApplicationTests.java
@@ -1,13 +1,28 @@
 package com.example.integration;
 
+import com.example.integration.config.ValuesProperties;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+
+import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest(properties = "spring.config.name=application-test")
 class IntegrationApplicationTests {
 
+	@Autowired
+	ValuesProperties valuesProperties;
+
 	@Test
-	void contextLoads() {
+	void loadValuesPropertiesOk() {
+		assertNotNull(valuesProperties.getLocalValue());
+		assertNotNull(valuesProperties.getApiKeyVault());
+		assertNotNull(valuesProperties.getApiKeyVault3());
+		assertNotEquals("", valuesProperties.getApiKeyVault3());
+		assertNotEquals("", valuesProperties.getApiKeyVault());
 	}
 
 }
